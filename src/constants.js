e0d325f58a5aed12d13379554685a4e34689cb72
ntun7729 <ntun77.19@gmail.com>
2026-05-16 17:45:32 +0630

    Add shared Worker constants

---
+++ src/constants.js
@@ -0,0 +1,26 @@
+export const VERSION = "up2-vless-worker-2026-05-16";
+
+export const CF_IPS = [
+  "104.16.0.0",
+  "104.17.0.0",
+  "104.18.0.0",
+  "104.19.0.0",
+  "104.20.0.0",
+  "104.21.0.0",
+  "172.64.0.0",
+  "172.65.0.0",
+  "172.66.0.0",
+  "172.67.0.0",
+];
+
+export const DEFAULT_DOH = [
+  "https://cloudflare-dns.com/dns-query",
+  "https://dns.google/dns-query",
+  "https://dns.quad9.net/dns-query",
+];
+
+export const DEFAULT_DNS_TCP = ["1.1.1.1:53", "8.8.8.8:53", "9.9.9.9:53"];
+
+export const WS_OPEN = 1;
+export const WS_CLOSING = 2;
+export const DNS_CACHE_LIMIT = 256;
